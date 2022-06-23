973d5cf35cee4fda50146d459114ef0026c80985
Debojyoti Koley <debojyoti.koley.dk@gmail.com>
2022-06-23 17:30:26 +0530

    search

---
+++ routes/authors.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const { route } = require('.')
+const router = express.Router()
+const Author = require('../models/author')
+
+
+// All Authors Route
+router.get('/', async(req,res) =>{
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name,'i')
+    }
+    try{
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index' , {authors : authors,
+        searchOptions:req.query})
+    }catch{
+        res.redirect('/')
+    }
+    
+})
+
+
+// New Authors Route
+router.get('/new', (req,res) => {
+    res.render('authors/new', {author: new Author()})
+})
+
+// Create New Author Route
+router.post('/', async(req,res) =>{
+    const author = new Author({
+        name: req.body.name
+    })
+
+
+    try{
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    }
+    catch{
+        res.render('authors/new', {
+            author:author,
+            errorMessage: 'Error creating new Author'
+        })
+    }
+
+    // res.send(req.body.name)
+
+    // author.save((err,newAuthor) =>{
+    //     if (err){
+    //         res.render('authors/new', {
+    //             author:author,
+    //             errorMessage: 'Error creating new Author'
+    //         })
+    //     }
+    //     else{
+    //         // res.redirect(`authors/${newAuthor.id}`)
+    //         res.redirect(`authors`)
+    //     }
+    // })
+    // res.send(req.body.name) 
+})
+
+ 
+module.exports = router
